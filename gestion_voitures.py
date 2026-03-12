1469457d2ad50879cfd847246bad64783010dcf5
ERNICE778 <erniceken3@gmail.com>
2026-03-11 23:10:05 -0400

    ajout de la voiture a la liste

--- gestion_voitures.py
+++ gestion_voitures.py
@@ -1,8 +1,71 @@
-class voiture:
+class Voiture:
     def __init__(self,matricule,marque,couleur):
         self.matricule = matricule
         self.marque=marque
         self.couleur=couleur 
 
-      
+    def afficher_informations(self):
+        print(f"les caracteristiques de cette voiture sont: matricule :{self.matricule},marque  :{self.marque},couleur: {self.couleur}")  
+
+
+
+
+class Parc:
+    def __init__(self,id,adresse,capacite):
+        self.id=id
+        self.adresse=adresse
+        self.capacite=capacite
+        self.listeVoitures=[]
+
+
+    def calculerNbrPlacesLibres(self):
+        return self.capacite -len(self.listeVoitures)
+    
+    def entrerVoiture(self,voiture):
+        if self.calculerNbrPlacesLibres() <=0:
+            print(f"Impossible d'ajouter cette voiture:le parc est deja plein.")
+            return
         
+
+        etat_vehicule= False
+        for  v in self.listeVoitures:
+            if v.matricule == voiture.matricule:
+                etat_vehicule = True
+            else :
+                etat_vehicule =False
+
+        if etat_vehicule == True:
+            print (f"la voiture est presente danc le parc donc impossible de l' ajouter dans le parc")
+        else :
+            print(f"voiture ajoutee avec succes: la voiture {voiture.matricule} est entree dans le parc")    
+
+    def sortirVoiture(self,voiture):
+        retraitVoiture = None
+        for v in self.listeVoitures:
+            if v.matricule == voiture.matricule:
+                retraitVoiture = v
+                break
+        if retirerVoiture:
+            self.listeVoitures.remove(retraitVoiture)    
+            print(f"la voiture {voiture.matricule} est sortie avec succes")
+        else :
+            print (f"la voiture {voiture.matricule} n'est pas presente dans le parc")    
+
+
+
+
+parc_boreal = Parc(1,"60 Distillery",3)
+
+v1 =Voiture("ABC-12345","ferrari","verte")
+v2 =Voiture("DEF-45668","tesla","blanc")
+v3 =Voiture("GHI-78955","mercedes","bleue")
+v4 =Voiture("JKL-10152","ford","grise")
+
+parc_boreal.entrerVoiture(v1)
+parc_boreal.entrerVoiture(v2)
+parc_boreal.entrerVoiture(v3)
+parc_boreal.entrerVoiture(v4)
+
+parc_boreal.sortirVoiture(v3)
+
+parc_boreal.entrerVoiture(v4)
